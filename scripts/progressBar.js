aa977c2c20b1fd584e6df91cadea2299b369296e
Aadithyaa Eeswaran <eaadithyaa@gmail.com>
2023-06-24 12:14:08 +0530

    JS Progress Bars

---
+++ scripts/progressBar.js
@@ -0,0 +1,14 @@
+
+window.addEventListener("load", (function () {
+	let bars = document.getElementsByClassName("progress-bar")
+	for (bar of bars) {
+		let child = bar.getElementsByClassName("progress-text")[0]
+		let num = parseInt(child.innerText)
+		console.log(num)
+		let bg = `radial-gradient(closest-side, var(--sidebar-color) 79%, transparent 80% 100%), conic-gradient(var(--progress-complete) ${num}%, var(--progress-left) 0)`
+		bar.style.background = bg;
+		console.log(bg);
+	}
+}
+));
+
