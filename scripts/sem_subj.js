f5b724fe974b8a119260cee725044894706206e2
Shreyas Jenner <shreyasjenner4@gmail.com>
2023-06-28 16:17:57 +0530

    created semester page

---
+++ scripts/sem_subj.js
@@ -0,0 +1,17 @@
+function handleSubjectClick(evt) {
+	let e = evt.currentTarget
+	const txt = e.childNodes[1].innerText
+	const val = txt.toLowerCase()
+	window.open(`../subjects/${path}/${val}.html`, "frame")
+}
+
+window.addEventListener("load", function() {
+	let subjs = document.getElementsByClassName("subj-container")
+	for (let subj of subjs) {
+		subj.addEventListener("click", handleSubjectClick)
+	}
+})
+
+var path = window.location.pathname
+path = path.split("/").pop()
+path = path.split(".")[0]
--- scripts/semesterLink.js
+++ scripts/semesterLink.js
@@ -3,12 +3,12 @@ function handleSemClick(evt) {
 	let e = evt.currentTarget
 	const txt = e.childNodes[1].innerText
 	const val = txt.split(" ")[1].toLowerCase()
-	window.open(`/semesters/sem_${val}`, "frame")
+	window.open(`semesters/sem_${val}.html`, "frame")
 }
 
 window.addEventListener("load", function() {
 	let sems = document.getElementsByClassName("sem-container")
-	for (sem of sems) {
+	for (let sem of sems) {
 		sem.addEventListener("click", handleSemClick)
 	}
 })
